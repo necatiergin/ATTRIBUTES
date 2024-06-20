8e41007f2bcddcf5d1abb285ac901d1f6752b491
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-06-20 10:18:12 +0300

    Update noreturn_01.cpp

--- noreturn/noreturn_01.cpp
+++ noreturn/noreturn_01.cpp
@@ -2,3 +2,12 @@
 //Bildirilen ya da tanımlanan fonksiyon çağıran koda geri dönüş sağlamayacak.
 
 [[noreturn]] void exit(int status);
+
+
+/*
+In order to return functions, compilers have to include some machine instructions in the generated code. For example, in many processors the machine instruction "ret" is used to return a function. However, this "ret" machine instruction alone may not be sufficient. The compiler may have to reload some registers with the input values before returning. 
+Therefore, the compiler, realizing that the function will not return, may not add this code to the function. This means more efficient code generation. 
+On the other hand, if the compiler realizes that the code will not return in the function call, it can make some optimizations there as well. 
+[[noreturn]] is also useful for understanding the code.
+
+*/
