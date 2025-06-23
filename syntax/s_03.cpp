5f4a4c798c011bcca8eba5c2d0661e4fdfa62271
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-06-23 18:28:29 +0300

    Delete syntax/s_03.cpp

--- syntax/s_03.cpp
+++
@@ -1,9 +0,0 @@
-int func()
-{
-	int ar[] = { 1, 3, 5, 7 };
-
-	//return ar [[]{return 1; }()]; //error
-	//return ar [ []{return 1; }()]; //still error
-	return ar[([] {return 1; }())]; //valid
-}
-
